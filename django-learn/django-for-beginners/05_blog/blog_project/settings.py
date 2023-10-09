e5a28698ac1ae4795daeafed50e22ed5a548a731
Igorswrk <Igorsantiago.work@gmail.com>
2023-10-09 17:41:52 -0300

    pag-163bookdjango create login & logout w/ superuser

--- django-learn/django-for-beginners/05_blog/blog_project/settings.py
+++ django-learn/django-for-beginners/05_blog/blog_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',  # new app
+    'blog.apps.BlogConfig',
+    'Accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,5 +124,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
