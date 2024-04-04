427c6879cb0445fabb9365dd1eee7ae00b710e07
Igorswrk <Igorsantiago.work@gmail.com>
2024-04-03 21:24:48 -0300

    [DFBBW] Adding signup views, form - Newspaper (CAP-9)

--- django-learn/django-for-beginners/06_newspaper/users/views.py
+++ django-learn/django-for-beginners/06_newspaper/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from .forms import CustomUserCreationFrom
 
 # Create your views here.
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationFrom
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy('login')
